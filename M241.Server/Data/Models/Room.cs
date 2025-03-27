5b338d393b0f38ef577fd90b33be44554c51c444
Dominic <dominic_tosku@kauz.ch>
2025-03-27 11:04:04 +0100

    feat: add additional data and mac address to client

--- M241.Server/Data/Models/Room.cs
+++ M241.Server/Data/Models/Room.cs
@@ -5,6 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Humidity { get; set; }
+        public int Temperature { get; set; }
+        public int AQI { get; set; }
+        public int NO2 { get; set; }
+        public int O3 { get; set; }
         public ClientDevice? Client { get; set; }
         public int ClientId { get; set; }
     }
--- M241.Server/Migrations/20250327095321_AdditionalRoomData.Designer.cs
+++ M241.Server/Migrations/20250327095321_AdditionalRoomData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using M241.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace M241.Server.Migrations
 {
     [DbContext(typeof(AeroSenseDbContext))]
-    partial class AeroSenseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250327095321_AdditionalRoomData")]
+    partial class AdditionalRoomData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -93,10 +96,6 @@ namespace M241.Server.Migrations
                         .HasColumnType("integer");
 
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
-
-                    b.Property<string>("MACAddr")
-                        .IsRequired()
-                        .HasColumnType("text");
 
                     b.Property<string>("Name")
                         .IsRequired()
