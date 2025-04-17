4e060df712673512c58d0408a9f5eaad11024955
Johannes Zeller <jo.zeller@protonmail.com>
2025-04-17 11:21:54 +0200

    adjust config

--- M241.Firmware/config.h
+++ M241.Firmware/config.h
@@ -1,5 +1,6 @@
-#define WIFI_SSID "JzelAdmin"
+#define WIFI_SSID "INF-LAB"
 #define MQTT_USER "mosquitto"
-#define MQTT_HOST "192.168.165.222"
+#define MQTT_HOST "172.18.13.22"
 #define MQTT_PORT 1883
 #define MQTT_QUEUE "room/data"
+#define DEBUGGING_ENABLED true
