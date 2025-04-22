491c55fdf2f597b9986e56ac2e96d2345924f80d
Dominic <dominic_tosku@kauz.ch>
2025-04-22 10:38:58 +0200

    fix: lowercase json properties

--- M241.Server/Data/Models/RoomData.cs
+++ M241.Server/Data/Models/RoomData.cs
@@ -1,14 +1,31 @@
-﻿namespace M241.Server.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace M241.Server.Data.Models
 {
     public class RoomData
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("humidity")]
         public float Humidity { get; set; }
+
+        [JsonPropertyName("temperature")]
         public float Temperature { get; set; }
+
+        [JsonPropertyName("pressure")]
         public float Pressure { get; set; }
+
+        [JsonPropertyName("gas")]
         public float Gas { get; set; }
+
+        [JsonPropertyName("timeStamp")]
         public DateTime TimeStamp { get; set; }
+
+        [JsonPropertyName("room")]
         public Room? Room { get; set; }
+
+        [JsonPropertyName("roomId")]
         public int RoomId { get; set; }
     }
 }
