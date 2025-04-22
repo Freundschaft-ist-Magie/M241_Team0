101f1b7bf5692f84014276cc2cc2375a0d5e769e
Dominic <dominic_tosku@kauz.ch>
2025-04-22 14:06:28 +0200

    feat: user controller

---
+++ M241.Server/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using M241.Server.Data;
+using M241.Server.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace M241.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly AeroSenseDbContext _context;
+
+        public UsersController(UserManager<AppUser> userManager, AeroSenseDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _context.Users.FirstOrDefaultAsync(e => e.Id == id));
+        }
+
+        // POST: api/users
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] RegisterUserDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new AppUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+                return Ok($"User '{model.UserName}' created successfully.");
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(error.Code, error.Description);
+
+            return BadRequest(ModelState);
+        }
+    }
+
+    public class RegisterUserDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
