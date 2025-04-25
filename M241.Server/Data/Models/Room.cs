2ab2e491177c359805cf3f4d7bc62f3ba4384f69
Justin <justin-urbanek@bluewin.ch>
2025-04-25 11:18:17 +0200

    feat: room endpoints return values in lowercase

--- M241.Server/Data/Models/Room.cs
+++ M241.Server/Data/Models/Room.cs
@@ -1,9 +1,14 @@
-﻿namespace M241.Server.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace M241.Server.Data.Models
 {
     public class Room
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("macAddress")]
         public string MACAddress { get; set; } = string.Empty;
+        [JsonPropertyName("isBurning")]
         public bool IsBurning { get; set; }
     }
 }
