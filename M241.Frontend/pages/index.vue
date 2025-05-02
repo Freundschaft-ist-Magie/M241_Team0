ec43ace89517113136cc34313b53b8f43c44bf50
Maxoso41 <40214662+Maxoso41@users.noreply.github.com>
2025-05-02 11:29:53 +0200

    feat: update grid layout to accommodate additional statistic card

--- M241.Frontend/pages/index.vue
+++ M241.Frontend/pages/index.vue
@@ -483,7 +483,7 @@ onUnmounted(() => {
     <!-- Statistic Cards -->
     <div
       v-if="selectedRoom"
-      class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 items-center"
+      class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 items-center"
     >
       <StatisticCard
         v-for="card in cards"
