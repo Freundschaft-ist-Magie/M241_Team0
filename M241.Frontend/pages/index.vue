cd9576423977313ced9871fd16c40cc2c58856b3
Justin <justin-urbanek@bluewin.ch>
2025-05-07 11:43:46 +0200

    feat: dissource charts to component

--- M241.Frontend/pages/index.vue
+++ M241.Frontend/pages/index.vue
@@ -521,51 +521,7 @@ onUnmounted(() => {
         <!-- Room Table -->
         <RoomTable :room-data="roomsHistory" :selected-room="selectedRoom" class="mt-4" />
 
-        <!-- Mobile Tabs for Charts -->
-        <div class="mt-4 block sm:hidden">
-          <Tabs value="0">
-            <TabList
-              class="flex border-b border-gray-200 dark:border-gray-700 overflow-x-auto"
-            >
-              <Tab v-for="tab in tabs" :key="tab.title" :value="tab.value">
-                {{ tab.title }}
-              </Tab>
-            </TabList>
-            <TabPanels class="p-0!">
-              <TabPanel v-for="tab in tabs" :key="tab.value" :value="tab.value">
-                <div class="mt-4 flex flex-col gap-4">
-                  <StatisticDiagram
-                    v-if="charts[tab.dayChart]"
-                    :title="chartTitles[tab.dayChart]"
-                    :chartData="charts[tab.dayChart].data"
-                    :chartOptions="charts[tab.dayChart].options"
-                    chartType="line"
-                  />
-                  <p v-else class="text-sm text-gray-500 text-center py-4">
-                    Diagramm nicht verfügbar.
-                  </p>
-                </div>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </div>
-
-        <!-- Desktop Day Charts -->
-        <div class="mt-4 hidden sm:flex flex-wrap gap-4">
-          <StatisticDiagram
-            v-for="(chart, index) in charts"
-            :key="'day-' + index"
-            :title="chartTitles[index]"
-            :chartData="chart.data"
-            :chartOptions="chart.options"
-            chartType="line"
-            class="flex-grow w-full md:w-[calc(50%-0.5rem)] min-w-[250px]"
-          />
-          <!-- Placeholder if charts array is empty but history exists (edge case) -->
-          <p v-if="charts.length === 0" class="w-full text-center text-gray-500 py-4">
-            Keine Diagramme zum Anzeigen.
-          </p>
-        </div>
+        <ChartsSection :tabs="tabs" :charts="charts" :chart-titles="chartTitles" />
       </div>
     </div>
   </div>
