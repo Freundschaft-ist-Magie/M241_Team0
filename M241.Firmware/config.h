23bcdc1f58b99accd48acdc621c32ebd6cb2d4d3
dominictosku <dominic.tosku@gmail.com>
2025-05-17 17:22:57 +0200

    change firmware config to new url

--- M241.Firmware/config.h
+++ M241.Firmware/config.h
@@ -1,6 +1,6 @@
 #define WIFI_SSID "INF-LAB"
 #define MQTT_USER "mosquitto"
-#define MQTT_HOST "rpi.m241.api64.de"
+#define MQTT_HOST "aircheck.jzel.ch"
 #define MQTT_PORT 8883
 #define MQTT_QUEUE "room/data"
 #define MQTT_PING_TOPIC "room/ping"
